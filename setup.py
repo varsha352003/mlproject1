af81926734d592a10f83964cd693ae003dac010d
varsha352003 <varsha352003@gmail.com>
2024-07-23 15:46:27 +0530

    first setup commit

---
+++ setup.py
@@ -0,0 +1,37 @@
+from setuptools import find_packages,setup
+from typing import List
+
+#HYPEN_E_DOT='-e .'
+
+def get_requirements(file_path:str)->List[str]:
+    '''
+    this function will return the list of requirements
+    '''
+    requirements=[]
+
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace('\n',"") for req in requirements]
+        '''if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)'''
+    return requirements
+
+
+
+
+
+
+
+
+setup(
+name='mlproject1',
+version='0.0.1',
+author='Varsha',
+author_email='varsha352003@gmail.com',
+packages=find_packages(),
+install_requires=get_requirements('requirements.txt') ##creating a fun to get all our installations
+
+
+
+
+)
